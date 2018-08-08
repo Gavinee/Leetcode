fe2a349715fec4a78ae0d08443635d6964971d4a
Gavinee <1780406643@qq.com>
2018-08-08 18:51:24 +0800

    dict  映射

--- 006  Z字形变换.py
+++ 006  Z字形变换.py
@@ -26,6 +26,13 @@ P     I
 """
 __author__ = 'Qiufeng'
 
+"""
+方法一：
+    找z子形与横行读取之间的映射关系，通过list存储之间的映射关系
+    此方法可行，但在拼接成字符串时，无法删除已经使用的元素，造成
+    程序在运行时可能会超时。
+"""
+
 class Solution:
     def convert(self, s, numRows):
         """
@@ -74,4 +81,58 @@ class Solution:
                     k+=1
             j+=1
         return tt
-"""                
+"""  
+
+"""
+方法二：
+    通过对方法一的优化，利用dict来储存二者之间的映射关系。
+    并在遍历dict时，可以删除已经使用的元素，使程序效率大大
+    增加，方法一运行时间是1740ms，而方法而运行时间缩短到
+    1170ms.
+    
+    Python -- 遍历字典时删除元素技巧
+    
+    d = {'a':1, 'b':0, 'c':1, 'd':0}
+    keys = list(d.keys())
+    for key in keys:
+        del(d[key])
+"""
+
+class Solution:
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        tt = ""
+        #Col = []
+        dict1 = {}
+        if len(s)<=numRows:
+            return s
+        
+        if numRows <2:
+            return s
+        
+        for i in range(0,len(s),1):
+            position = numRows - 1
+            row = i//position#列
+            temp = i%(2*position)
+            col = position - abs(temp-position)  #行
+            dict1[i] = col
+            #Col.append(col)   
+            position = 0
+
+            
+        k = 0
+        for j in range(0,numRows,1):
+            
+            keys = list(dict1.keys())
+            for key in keys:
+                if dict1[key] == k:
+                    tt+=s[key]
+                    del(dict1[key])
+            k+=1
+            j+=1
+        return tt
+    
