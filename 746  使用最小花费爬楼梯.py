c9542efbc3b88953eaf1ed4ec697c66cb77704e7
Gavinee <1780406643@qq.com>
2018-08-19 13:09:00 +0800

    动态规划

--- 746  使用最小花费爬楼梯.py
+++ 746  使用最小花费爬楼梯.py
@@ -62,3 +62,19 @@ class Solution(object):
             self.dynamicProgramming(cost,step,minCost,temp)
             temp = temp - cost[step]
             step = step - 1
+
+            
+#                       通过的程序       
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        dp = {}
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2,len(cost)):
+            dp[i] = min(dp[i-2]+cost[i],dp[i-1]+cost[i])
+        return min(dp[len(cost)-1],dp[len(cost)-2])
+    
