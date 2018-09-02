076a793cc784ab8f6f3b587d50dad3fb664f73ee
落落佳一、 <1780406643@qq.com>
2018-09-02 16:40:03 +0800

    Create 105  从前序与中序遍历序列构造二叉树.py

---
+++ 105  从前序与中序遍历序列构造二叉树.py
@@ -0,0 +1,42 @@
+"""
+根据一棵树的前序遍历与中序遍历构造二叉树。
+
+注意:
+你可以假设树中没有重复的元素。
+
+例如，给出
+
+前序遍历 preorder = [3,9,20,15,7]
+中序遍历 inorder = [9,3,15,20,7]
+返回如下的二叉树：
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+"""
+__author__= 'Qiufeng'
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder:
+            return None
+        root = TreeNode(preorder[0])
+
+        n = inorder.index(root.val)
+        root.left = self.buildTree(preorder[1:n+1],inorder[:n])
+        root.right = self.buildTree(preorder[n+1:],inorder[n+1:])
+        return root
