bb48985b92831b45e7f6652f5834e10578928e3a
落落佳一、 <1780406643@qq.com>
2018-09-27 17:32:11 +0800

    Create 066  加一.cpp

---
+++ 066  加一.cpp
@@ -0,0 +1,51 @@
+/*
+  给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+
+  最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+
+  你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+  示例 1:
+  输入: [1,2,3]
+  输出: [1,2,4]
+  解释: 输入数组表示数字 123。
+  
+  示例 2:
+  输入: [4,3,2,1]
+  输出: [4,3,2,2]
+  解释: 输入数组表示数字 4321。
+*/
+
+//author = Qiufeng
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+        int index = digits.size()-1;
+        int on = 0;
+        while(index>=0)
+        {
+            
+            if(index == digits.size()-1)
+                digits[index] = digits[index] + 1;
+            digits[index] = digits[index]+on;
+            on = digits[index]/10;
+            if(on<1)
+                return digits;
+            digits[index]%=10;
+            if(index==0)
+            {
+                vector<int> temp;
+                temp.push_back(1);
+                for(int i = 0;i<digits.size();i++)
+                {
+                    temp.push_back(digits[i]);
+                }
+                return temp;
+            }
+            index--;
+        }
+        return digits;
+    }
+};
